1ef0401492297436a8e308a94978aa2f43e85ce8
Yevhenii <146078178+EvheniiOrza@users.noreply.github.com>
2024-10-26 16:22:42 +0300

    commit list calculators projects

--- stardue_walley_project_webapp/settings.py
+++ stardue_walley_project_webapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,10 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Директорія, де ваші статичні файли
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Використовується для режиму розробки
